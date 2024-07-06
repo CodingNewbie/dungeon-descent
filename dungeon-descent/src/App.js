75170aa992b2a9e9b90ce88bd1aa947dc8e127fc
Adam <adamrayschmidt@gmail.com>
2024-07-06 14:47:13 -0700

    Update style for game menu.

--- dungeon-descent/src/App.js
+++ dungeon-descent/src/App.js
@@ -6,14 +6,27 @@ function App() {
   const [events, setEvents] = useState([]);
   const eventsEndRef = useRef(null);
 
+  const messages = [
+    "There is nothing in this area.",
+    "You found a corpse.",
+    "You found a door.",
+    "You moved to the next floor.",
+    "You explored and found nothing.",
+    "You found an empty chest.",
+    "You encountered a Goblin.",
+    "You encountered an Orc.",
+    "You encountered a Skeleton Archer."
+  ];
+
   useEffect(() => {
     eventsEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [events]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setEvents(prevEvents => [...prevEvents, `New event at ${new Date().toLocaleTimeString()}`]);
-    }, 2000);
+      const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+      setEvents(prevEvents => [...prevEvents, randomMessage]);
+    }, 500);
 
     return () => clearInterval(interval);
   }, []);
