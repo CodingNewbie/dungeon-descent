6f144b46c86b141d1f24b395b0ea5919a3a92b17
Adam <adamrayschmidt@gmail.com>
2024-07-09 14:11:45 -0700

    Add health bar shake animation when taking damage.

--- dungeon-descent/src/components/HealthBar.js
+++ dungeon-descent/src/components/HealthBar.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/HealthBar.css';
 
 function HealthBar({ current, total }) {
+  const [isShaking, setIsShaking] = useState(false);
+
   const healthPercentage = (current / total) * 100;
   const widthStyle = current > 0 ? `${healthPercentage}%` : '0%';
   const paddingStyle = current > 0 ? '5px' : '0';
 
+  useEffect(() => {
+    if (current < total) {
+      setIsShaking(true);
+      const timer = setTimeout(() => {
+        setIsShaking(false);
+      }, 500); // Duration of the shake animation
+      return () => clearTimeout(timer);
+    }
+  }, [current, total]);
+
   return (
-    <div className="HealthBar">
+    <div className={`HealthBar ${isShaking ? 'shake' : ''}`}>
       <div
         className="HealthBar-inner"
         style={{ width: widthStyle, paddingLeft: paddingStyle }}
