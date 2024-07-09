4760a635694c99f9c83dbcedd30b9a8368551314
Adam <adamrayschmidt@gmail.com>
2024-07-08 19:29:58 -0700

    Add React Router and landing page.

--- dungeon-descent/src/App.js
+++ dungeon-descent/src/App.js
@@ -1,14 +1,20 @@
-// src/App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 import Game from './components/Game';
 import './styles/App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <Game />
-    </div>
+    <Router basename="/dungeon-descent">
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
