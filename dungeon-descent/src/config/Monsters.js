f1dd02c1a1150ec7017da2e90b8540d233a29956
Adam <adamrayschmidt@gmail.com>
2024-07-09 19:55:55 -0700

    Change monster image sizing and add skeleton.

--- dungeon-descent/src/config/Monsters.js
+++ dungeon-descent/src/config/Monsters.js
@@ -1,7 +1,8 @@
 const monsters = [
     { type: 'mimic', probability: 0.2 },
-    { type: 'goblin', probability: 0.4 },
-    { type: 'kobold', probability: 0.5 }
+    { type: 'goblin', probability: 0.2 },
+    { type: 'kobold', probability: 0.3 },
+    { type: 'skeleton', probability: 0.3 }
   ];
   
   export default monsters;
