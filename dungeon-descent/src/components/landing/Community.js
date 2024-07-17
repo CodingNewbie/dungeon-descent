c91b36345920f278a2312d1a05622cf64bd3cf1d
notfreephilly <not.philyoder93@gmail.com>
2024-07-17 11:29:58 -0700

    installed react-scroll-parallax and included image assets

--- dungeon-descent/src/components/landing/Community.js
+++ dungeon-descent/src/components/landing/Community.js
@@ -3,7 +3,16 @@ import React from "react";
 function Community() {
     return (
         <div>
-            <h2>Join the Descent community!</h2>
+            <div className='LandingPage-community'>
+                <h2>Join our community!</h2>
+                <p>Dive into discussions of strategy, skill, and lively banter within our vibrant Descent community! Connect with fellow adventurers, share tactics, and explore the depths together. Click the links below to join the conversation and become part of our growing community!</p>
+                {/* Import font-awesome and use icons here */}
+                <div className='community-links'>
+                    {/* links will go here */}
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Community;
--- dungeon-descent/src/components/landing/Header.js
+++ dungeon-descent/src/components/landing/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../../styles/landing/LandingPage.css';
+
 
 
 function Header() {
     return (
         <div>
-            <h1>Your Descent awaits...</h1>
+            <header className="LandingPage-header">
+                <h1>Your Descent awaits...</h1>
+                <Link to="/game">
+                    <button>Start Game</button>
+                </Link>
+            </header>
         </div>
     )
 }
