48e657da9fb4292d5dbc7a1659e78076cd689ce7
notfreephilly <not.philyoder93@gmail.com>
2024-07-17 20:00:07 -0700

    created parallax effect for landing page, added minimal styling to the effect

--- dungeon-descent/src/components/landing/Hero.js
+++ dungeon-descent/src/components/landing/Hero.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../../styles/landing/LandingPage.css';
+import '../../styles/landing/Hero.css';
 
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 
 function Hero() {
     return (
-        <div>
+        <div className="hero-body">
             <h1>Your Descent awaits...</h1>
             <Link to="/game">
                 <button>Start Game</button>
--- dungeon-descent/src/components/landing/LandingPage.js
+++ dungeon-descent/src/components/landing/LandingPage.js
@@ -5,24 +5,38 @@ import Hero from './Hero';
 import About from './About';
 import Community from './Community';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import background from '../../assets/background.png';
+import mainbackground from '../../assets/mainbackground.png';
+import foreground from '../../assets/foreground.png';
+import midlayer from '../../assets/midlayer.png';
+
 
 
 function LandingPage() {
     return (
         <div className="LandingPage">
-            <Parallax pages={4}>
+            <Parallax pages={3}>
+                <ParallaxLayer
+                    offset={0}
+                    speed={0.1}
+                    style={{
+                        backgroundImage: `url(${foreground})`,
+                        backgroundSize: 'cover'
+                    }}
+                />
 
-                <ParallaxLayer>
+
+                <ParallaxLayer offset={0.3} speed={6} className='hero-about'>
                     <Hero />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={1} speed={0.5}>
-                    <About />
 
+                <ParallaxLayer offset={1} speed={1}>
+                    <About />
                 </ParallaxLayer>
+
                 <ParallaxLayer offset={2} speed={1}>
                     <Community />
-
                 </ParallaxLayer>
             </Parallax>
 
