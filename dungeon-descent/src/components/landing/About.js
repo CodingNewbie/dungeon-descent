a3dd1f3ed072f4b7f7f7925966ca3a5f5809b42b
notfreephilly <not.philyoder93@gmail.com>
2024-07-29 16:14:05 -0700

    restructured landing page using css, took out parrallax effect, re-designed navbar

--- dungeon-descent/src/components/landing/About.js
+++ dungeon-descent/src/components/landing/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return (
-        <div>
+        <div id="about">
             <div className='LandingPage-about'>
                 <h2>About</h2>
                 <p>Embark on an epic journey in Descent, a thrilling 2D roguelite dungeon crawler. Begin your adventure at the summit, making your way down through treacherous depths filled with unknown challenges and fearsome foes. Each descent brings new surprises and rewards, pushing your skills and strategy to the limit. Can you survive the descent and uncover the secrets that lie beneath? Prepare yourself for a relentless adventure where every decision matters and every victory brings you closer to mastering the depths.</p>
--- dungeon-descent/src/components/landing/LandingPage.js
+++ dungeon-descent/src/components/landing/LandingPage.js
@@ -7,45 +7,17 @@ import Hero from './Hero';
 import About from './About';
 import Community from './Community';
 import GameInfo from './GameInfo';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import background from '../../assets/background.png';
-import mainbackground from '../../assets/mainbackground.png';
-import foreground from '../../assets/foreground.png';
-import midlayer from '../../assets/midlayer.png';
 
 function LandingPage() {
     return (
         <div className="LandingPage">
             <Nav />
-            <Parallax pages={4}>
-                <ParallaxLayer
-                    offset={0}
-                    speed={0.1}
-                    style={{
-                        backgroundImage: `url(${foreground})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    }}
-                />
+            <Hero />
+            <About />
 
-                <ParallaxLayer offset={0.3} speed={0.6} className='hero-about'>
-                    <Hero />
-                </ParallaxLayer>
+            <GameInfo />
 
-                <ParallaxLayer offset={1} speed={1}>
-                    <About />
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={1.9} speed={1}>
-                    <GameInfo />
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={2.2} speed={1.2}>
-                    <Community />
-                </ParallaxLayer>
-
-
-            </Parallax>
+            <Community />
             <Footer />
         </div>
     );
